27627530b3a63f7f870e18eef089d774fb5dadd5
ibrunton <wolfshift@gmail.com>
2011-02-21 14:04:31 -0400

    Fixed a couple copying errors in POD

--- tumblimg.pl
+++ tumblimg.pl
@@ -95,20 +95,18 @@ Report bugs to wolfshift@gmail.com
 
 Copyright 2011 Ian D. Brunton
 
-This file is part of Log.
-
-Log is free software: you can redistribute it and/or modify
+This file is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Log is distributed in the hope that it will be useful,
+This file is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Log.  If not, see <http://www.gnu.org/licenses/>.
+along with this file.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut  
 
